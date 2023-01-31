db9a8d0df586f8e5a0a92fbb2e0814df2378597e
max brewer <58602708+deepfates@users.noreply.github.com>
2023-01-30 17:43:41 -0700

    refined prompt

--- npc/prompts.py
+++ npc/prompts.py
@@ -9,14 +9,13 @@ class ChainSignature:
 
 
 NAME = """I am NPC, an advanced game-playing language model.
-My task is to generate a command for a text-based adventure game.
+My task is to win a text-based adventure game.
 """
 
 COMMANDS = """The game understands the following commands:
-Movement: north, south, east, west, northeast, northwest, southeast, southwest, up, down, look, save, restore, restart, verbose, score, diagnostic, brief, superbrief, quit (q), climb, g, go (direction), enter, in, out, hi/hello
+Movement: north, south, east, west, northeast, northwest, southeast, southwest, up, down, look, score, diagnostic, climb, go (direction), enter, in, out
 Item: get/take/grab (item), get/take/grab all, throw (item) at (location), open (container), open (exit), read (item), drop (item), put (item) in (container), turn (control) with (item), turn on (item), turn off (item), move (object), attack (creature) with (item), examine (object), inventory, eat, shout, close [Door], tie (item) to (object), pick (item), kill self with (weapon), break (item) with (item), kill (creature) with (item), pray, drink, smell, cut (object/item) with (weapon)
-Other: (none), Zork, f%&$/s@^#/damn, jump
-Wand (only if you have the wand): fall, fantasize, fear, feeble, fence, ferment, fierce, filch, fireproof, float, fluoresce, free, freeze, frobizz, frobnoid, frobozzle, fry, fudge, fumble
+Wand (only if I have the wand): fall, fantasize, fear, feeble, fence, ferment, fierce, filch, fireproof, float, fluoresce, free, freeze, frobizz, frobnoid, frobozzle, fry, fudge, fumble
 """
 
 SHEM = NAME + COMMANDS
@@ -32,12 +31,11 @@ Plan: Consider my overall goals and plan the next step
 Command: Generate command text
 ```
 Begin!
-```
-{chat_history}
-Game:{human_input}
-
+---
 Memories:{entities}
 
+{chat_history}
+Game:{human_input}
 NPC:"""
 
 
