e8850a55746fd0c8151c062abe5d20d37e4c2d13
max brewer <58602708+deepfates@users.noreply.github.com>
2023-02-02 18:23:29 -0700

    ws

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -32,7 +32,7 @@ let buttonsLoading = {
 }
 
 // connect to the server
-const socket = io('wss://0.0.0.0:8080', {
+const socket = io('ws://0.0.0.0:8080', {
     transports: ['websocket'],
     upgrade: false
 });
